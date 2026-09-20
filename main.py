be2e5f35d3bad51e8d35da95bccb252c0064edf6
empty2145 <tasybaevtamir@gmail.com>
2026-09-21 02:09:38 +0500

    import libraries and create the main fun

---
+++ main.py
@@ -0,0 +1,9 @@
+import subprocess
+import time
+import sys
+
+def ascii_video(video_path, width=80, fps=24):
+    height = int(width / 2)
+
+if __name__ == "__main__":
+    ascii_video("input.mp4", width=100, fps=24)
