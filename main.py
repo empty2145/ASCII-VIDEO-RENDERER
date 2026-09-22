504b4650ef471e87ef119f8d6923b52c03a14082
empty2145 <tasybaevtamir@gmail.com>
2026-09-22 23:11:20 +0500

    feat: setup FFmpeg to output raw RGb

--- main.py
+++ main.py
@@ -5,5 +5,21 @@ import sys
 def ascii_video(video_path, width=80, fps=24):
     height = int(width / 2)
 
+    ascii_chars = " .:-=+*#%@"
+
+    # ask dat thing
+    command = [
+        'ffmpeg',
+        '-i', video_path,
+        '-vcodec', 'rawvideo',
+        '-pix_fmt', 'rgb24',
+        '-r', str(fps),
+        '-s', f'{width}x{height}',
+        '-f', 'image2pipe',
+        '-loglevel', 'quiet',
+        '-'
+    ]
+    
+
 if __name__ == "__main__":
     ascii_video("input.mp4", width=100, fps=24)
